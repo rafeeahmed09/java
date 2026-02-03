3645c9e5a755787161a5d9fe7085f33b701109ed
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-02-03 16:03:20 +0530

    new code

--- FCollections/Map/LinkedHashMapDemo.java
+++ FCollections/Map/LinkedHashMapDemo.java
@@ -1,11 +1,13 @@
 package FCollections.Map;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LinkedHashMapDemo {
     public static void main(String[] args) {
-
+        
+        // Ordered - maintains insertion order.
         LinkedHashMap<String, Integer> liked = new LinkedHashMap<>(); // Double linked list
 
         liked.put("Apple", 10);
@@ -15,5 +17,14 @@ public class LinkedHashMapDemo {
         for (Map.Entry<String, Integer> entry : liked.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>(hashMap);
+        hashMap.put("Shibham", 91);
+        hashMap.put("Bob", 23);
+        hashMap.put("Akshit", 45);
+
+        Integer val = hashMap.getOrDefault("vipul", 0);
+        System.out.println(val);
     }
 }
--- Project/Domain/Customer.java
+++ Project/Domain/Customer.java
@@ -8,6 +8,6 @@ public class Customer {
     public Customer(String id, String name, String gemail) {
         this.id = id;
         this.name = name;
-        Gemail = gemail;
+        this.Gemail = gemail;
     }
 }
