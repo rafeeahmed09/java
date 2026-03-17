bfc084aec65648e7ee7a0c6640c2f7452060f6a8
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-03-17 16:05:19 +0530

    new code

---
+++ File_Handling/Delete.java
@@ -0,0 +1,19 @@
+package File_Handling;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+public class Delete {
+    static void main(String[] args) {
+        File myfile = new File("acb.txt");
+        if (myfile.delete()){
+            System.out.println("i have deleted" + myfile.getName());
+
+        }else{
+            System.out.println("Some problem occurred while");
+        }
+    }
+
+}
+
+
