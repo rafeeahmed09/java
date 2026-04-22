6ad8ec3475d99665a3c3304e87911098cd4b5ec9
Rafeeahmed09 <abdulrafee0078@gmail.com>
2026-04-22 17:20:46 +0530

    new code

---
+++ FCollections/Sets/Tree_set_Navigable.java
@@ -0,0 +1,27 @@
+package FCollections.Sets;
+
+import java.util.TreeSet;
+
+public class Tree_set_Navigable {
+     /* TreeSet
+           BST --> leftest most node --> smallest
+           rightest --> most node --> Largest
+     */
+    public static void main(String[] args) {
+         TreeSet<Integer> Set = new TreeSet<>();
+         Set.add(40);
+         Set.add(60);
+         Set.add(20);
+         Set.add(50);
+         Set.add(55);
+
+         // Navigable
+
+     }
+
+
+
+
+
+
+}
